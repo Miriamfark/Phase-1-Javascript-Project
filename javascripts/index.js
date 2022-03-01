6508e43be59a2dadf347775d9d6285696ad5ef50
Miriam Farkas <miriamjoshua00@gmail.com>
2022-03-01 10:14:49 +0200

    Added click event

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,40 @@
-alert('hi!')
+//NODE GETTERS
+const mainButton = () => document.getElementById('main-button')
+const sideButton = () => document.getElementById('side-button')
+const vegetableButton = () => document.getElementById('vegetable-button')
+
+//EVENT HANDLERS
+const renderMainsToPage = (e) => {
+    e.preventDefault()
+    alert("Meals button works!")
+}
+
+const renderSidesToPage = (e) => {
+    e.preventDefault()
+    alert("Side button works!")
+}
+
+const renderVegetablesToPage = (e) => {
+    e.preventDefault()
+    alert("Vegetables button works")
+}
+
+//EVENT LISTENERS
+const attachMainLinkEvent = () => {
+    mainButton().addEventListener('click', renderMainsToPage)
+}
+
+const attachSideLinkEvent = () => {
+    sideButton().addEventListener('click', renderSidesToPage)
+}
+
+const attachVegetableLinkEvent = () => {
+    vegetableButton().addEventListener('click', renderVegetablesToPage)
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    attachMainLinkEvent()
+    attachSideLinkEvent()
+    attachVegetableLinkEvent()
+}) 
