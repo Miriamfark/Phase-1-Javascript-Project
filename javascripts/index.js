7421b6baa1096fcafeca21d1d169eec6aa08771b
Miriam Farkas <miriamjoshua00@gmail.com>
2022-03-02 21:16:25 +0200

    added card

--- javascripts/index.js
+++ javascripts/index.js
@@ -9,28 +9,78 @@ const resetMainWrapper = () => {
     mainWrapper().innerHTML = ""
 }
 
-const createCard = () => {
-    <div class="row">
-    <div class="col s12 m7">
-      <div class="card">
-        <div class="card-image">
-          <img src="images/sample-1.jpg">
-          <span class="card-title">Card Title</span>
-        </div>
-        <div class="card-content">
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action">
-          <a href="#">This is a link</a>
-        </div>
-      </div>
-    </div>
-  </div>
+const createCard = (dish) => {
+
+  const divCard = document.createElement('div');
+  const divImage = document.createElement('div');
+  const image = document.createElement("IMG")
+  const span = document.createElement('span')
+  const divCardContent = document.createElement('div')
+  const pDescription = document.createElement("p");
+  const divCardAction = document.createElement('div');
+  const link1 = document.createElement("a")
+
+  divCard.className = "card";
+  divImage.className = "card-image"
+  span.className = "card-title";
+  divCardContent.className = "card-content"
+  divCardAction.className = "card-action"
+
+  image.setAttribute("src", "https://www.kosher.com/resized/recipe_list_item/t/a/Tamarind_chicken_W.jpg");
+  span.innerText = "Card Title";
+  pDescription.innerText = "some filler text"
+  link1.setAttribute("href", "#")
+  link1.innerText = "Click here!"
+
+  divImage.appendChild(image);
+  divImage.appendChild(span);
+  divCardContent.appendChild(pDescription)
+  divCardAction.appendChild(link1)
+  divCard.appendChild(divImage)
+  divCard.appendChild(divCardContent
+  divCard.appendChild(divCardAction))
+
+
+  //   <div class="row">
+  //   <div class="col s12 m7">
+  //     <div class="card">
+  //       <div class="card-image">
+  //         <img src="images/sample-1.jpg">
+  //         <span class="card-title">Card Title</span>
+  //       </div>
+  //       <div class="card-content">
+  //         <p>I am a very simple card. I am good at containing small bits of information.
+  //         I am convenient because I require little markup to use effectively.</p>
+  //       </div>
+  //       <div class="card-action">
+  //         <a href="#">This is a link</a>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </div>
+  return divCard
 }
 
 const addCards = () => {
+  const row = document.createElement("row")
+  row.className = "row"
+  dishes.forEach(dish => {
+    const column = addCard(dish)
 
+    row.appendChild(column)
+
+    mainWrapper.appendChild(row)
+  })
+}
+
+const addCard = () => {
+
+  const column = document.createElement("div");
+  column.className = "col s12 m7"
+
+  column.appendChild(createCard())
+
+ 
 }
 
 //EVENT HANDLERS
