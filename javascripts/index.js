6e1d447af28d7cf3ae9e89fe87b04394a71d08e3
Miriam Farkas <miriamjoshua00@gmail.com>
2022-03-02 15:18:23 +0200

    saved

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,21 +2,65 @@
 const mainButton = () => document.getElementById('main-button')
 const sideButton = () => document.getElementById('side-button')
 const vegetableButton = () => document.getElementById('vegetable-button')
+const mainWrapper = () => document.getElementById('main-container')
+
+//HELPER FUNCTIONS
+const resetMainWrapper = () => {
+    mainWrapper().innerHTML = ""
+}
+
+const createCard = () => {
+    <div class="row">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/sample-1.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+}
+
+const addCards = () => {
+
+}
 
 //EVENT HANDLERS
 const renderMainsToPage = (e) => {
     e.preventDefault()
-    alert("Meals button works!")
+    resetMainWrapper()
+
+    const h2 = document.createElement("h2")
+    h2.innerText = "Main Dishes"
+    mainWrapper().appendChild(h2)
 }
 
 const renderSidesToPage = (e) => {
     e.preventDefault()
-    alert("Side button works!")
+    resetMainWrapper()
+
+    const h2 = document.createElement("h2")
+    h2.innerText = "Side Dishes"
+    mainWrapper().appendChild(h2)
+
+    addCards()
 }
 
 const renderVegetablesToPage = (e) => {
     e.preventDefault()
-    alert("Vegetables button works")
+    resetMainWrapper()
+
+    const h2 = document.createElement("h2")
+    h2.innerText = "Vegetables, Soups, and Salads"
+    mainWrapper().appendChild(h2)
 }
 
 //EVENT LISTENERS
